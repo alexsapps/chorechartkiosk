0f998f6b92488d1eb3bc5c96f81d611522ce4a2f
Alexander Taylor <ajxtaylor@gmail.com>
2020-09-13 21:38:01 -0700

    Web app code cleanup / add comments

--- HtmlService.ts
+++ HtmlService.ts
@@ -14,8 +14,14 @@ const WEB_SHEET_ID = DEV_WEB_TEST ? TEST_SHEET_ID : SHEET_ID;
 // NOTE for below functions: GAS HtmlService does not accept Date objects
 // in inputs and outputs of these functions.
 
-function webappGetWeek(week: number) {
-  const report = getReport(WEB_SHEET_ID, new Date(week));
+// Note that "Audit Dates"
+
+/**
+ * Gets the chore statuses for the given week.
+ * @param auditDate The week to get data for, expressed as the due date (such that chores are due the next day at 5AM) in milliseconds since epoch.
+ */
+function webappGetWeek(auditDate: number) {
+  const report = getReport(WEB_SHEET_ID, new Date(auditDate));
 
   return {
     chores: report.chores
@@ -27,6 +33,12 @@ function webappGetWeek(week: number) {
     )};
 }
 
+/**
+ * Sets the status for the given chore.
+ * @param auditDate The week that chores are due, expressed as the due date (such that chores are due the next day at 5AM) in milliseconds since epoch.
+ * @param person The name of the person whose chore should be updated.
+ * @param status The status to set for this chore on this week.
+ */
 function webappUpdateStatus(auditDate: number, person: string, status: string) {
   const tracker = new TrackerModel(getChoresSheet(WEB_SHEET_ID));
 
